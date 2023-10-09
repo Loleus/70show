d507df9604969dce80f631cdbc246d8d6619bc85
Loleus <loleus@gmail.com>
2023-10-09 22:02:50 +0200

    add music control button

--- js/index.js
+++ js/index.js
@@ -1,87 +1,100 @@
-'use strict'
-import { logo, title } from "./fetchApi.js"
-import slides from "./slides.js"
+"use strict";
+import { logo, title } from "./fetchApi.js";
+import slides from "./slides.js";
 
-let k, wrapper, words, numbers, audio
-
+let k, wrapper, words, numbers, audio;
+let music = true;
 const lastSlide = () => {
-    setTimeout(() => wrapper.innerHTML = `${title}` + `${logo}`, 500);
-}
+  setTimeout(() => (wrapper.innerHTML = `${title}` + `${logo}`), 500);
+};
 const nextSlide = () => {
-    if ((k < slides.length)) {
-        setPage();
-        ++k;
-        setTimeout(nextSlide, 5000);
-    } else {
-        lastSlide();
-        setTimeout(() => wrapper.appendChild(resetButton()), 13000);
-        k = 0
-    };
+  if (k < slides.length) {
+    setPage();
+    ++k;
+    setTimeout(nextSlide, 5000);
+  } else {
+    lastSlide();
+    setTimeout(() => wrapper.appendChild(resetButton()), 13000);
+    k = 0;
+  }
 };
 const getOneLine = (i) => {
-    const oneLine = document.createElement('h1');
-    oneLine.style.fontSize = (k > 2 && i > 3 ? "2.5em" : "3.5em");
-    return oneLine
-}
+  const oneLine = document.createElement("h1");
+  oneLine.style.fontSize = k > 2 && i > 3 ? "2.5em" : "3.5em";
+  return oneLine;
+};
 const getOneChar = (char, index) => {
-    const oneChar = document.createElement('span');
-    oneChar.style.color = index % 2 == 0 ? "#f00" : null;
-    oneChar.classList.add('hidden');
-    oneChar.innerHTML = char;
-    return oneChar
-}
+  const oneChar = document.createElement("span");
+  oneChar.style.color = index % 2 == 0 ? "#f00" : null;
+  oneChar.classList.add("hidden");
+  oneChar.innerHTML = char;
+  return oneChar;
+};
 const lettersAnim = (oneChar, index, max) => {
+  setTimeout(() => {
+    oneChar.classList.remove("hidden");
     setTimeout(() => {
-        oneChar.classList.remove('hidden');
-        setTimeout(() => {
-            oneChar.classList.add('hidden');
-        }, max);
-    }, index * 160);
-}
+      oneChar.classList.add("hidden");
+    }, max);
+  }, index * 160);
+};
 const getMax = (numbers) => {
-    return Math.max(...numbers) * 300 + 500;
-}
+  return Math.max(...numbers) * 300 + 500;
+};
 const setPage = () => {
-    const slide = slides[k]
-    wrapper.innerHTML = `<h1 id="header">${slide.header}</h1>`
-    words = [];
-    numbers = [];
-    slide.text.forEach((word, i) => {
-        words.push([...word]);
-        numbers.push([word.length])
-        const oneLine = getOneLine(i);
-        wrapper.appendChild(oneLine);
-        words[i].map((char, index) => {
-            const oneChar = getOneChar(char, index)
-            oneLine.appendChild(oneChar);
-            lettersAnim(oneChar, index, getMax(numbers))
-        });
-    })
-}
+  const slide = slides[k];
+  wrapper.innerHTML = `<h1 id="header">${slide.header}</h1>`;
+  words = [];
+  numbers = [];
+  slide.text.forEach((word, i) => {
+    words.push([...word]);
+    numbers.push([word.length]);
+    const oneLine = getOneLine(i);
+    wrapper.appendChild(oneLine);
+    words[i].map((char, index) => {
+      const oneChar = getOneChar(char, index);
+      oneLine.appendChild(oneChar);
+      lettersAnim(oneChar, index, getMax(numbers));
+    });
+  });
+};
 const resetButton = () => {
-    const elem = document.createElement("h1");
-    elem.innerText = "S T A R T"
-    elem.id = "button"
-    elem.onclick = () => {
-        audio.play();
-        setTimeout(nextSlide, 600)
-    }
-    return elem
-}
+  const elem = document.createElement("h1");
+  elem.innerText = "S T A R T";
+  elem.id = "button";
+  elem.onclick = () => {
+    music ? audio.play() : audio.pause();
+    setTimeout(nextSlide, 600);
+  };
+  return elem;
+};
+const soundButton = () => {
+  const elem = document.createElement("h1");
+  elem.innerHTML = 'Music is <span style="color:lightgreen"<i>ON</i></span>';
+  elem.id = "sound";
+  elem.onclick = () => {
+    music=!music;
+    music
+      ? (elem.innerHTML = 'Music is <span style="color:lightgreen"><i>ON</i></span>')
+      : (elem.innerHTML = 'Music is <span style="color:red"><i>OFF</i></span>');
+  };
+  return elem;
+};
 const initSound = () => {
-    let url = "./assets/audio/loop_ziu5he.wav";
-    audio = document.querySelector('audio');
-    audio.src = url;
-    audio.style.oneLine = "none";
-    audio.loop = true;
-    audio.controls = false;
-    audio.oncanplay = () => {
-        k = 0;
-        wrapper = document.getElementById('root');
-        wrapper.appendChild(resetButton());
-    };
-}
+  let url = "./assets/audio/loop_ziu5he.wav";
+  audio = document.querySelector("audio");
+  audio.src = url;
+  audio.style.oneLine = "none";
+  audio.loop = true;
+  audio.controls = false;
+  audio.oncanplay = () => {
+    k = 0;
+    wrapper = document.getElementById("root");
+    wrapper.appendChild(resetButton());
+    wrapper.appendChild(soundButton());
+  };
+};
 const init = () => {
-    initSound();
-}
-init()
+  initSound();
+};
+init();
